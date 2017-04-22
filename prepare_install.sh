44b21651ae7b537b9c12ab7f27d7a14350a617a6
Tim Kleyersburg <tk@wacg.de>
2017-04-22 11:06:23 +0200

    Added dots

--- prepare_install.sh
+++ prepare_install.sh
@@ -5,7 +5,7 @@ rm -rf ./pref
 mkdir -p ./pref/{ssh,Tunnelblick,dnsmasq}
 
 cp -R "$HOME/.ssh/" "./pref/ssh/"
-echo "SSH settings and keys copied"
+echo "SSH settings and keys copied."
 
 # PhpStorm settings (version needs to be adjusted as needed)
 phpstorm="PhpStorm2017.1"
@@ -15,22 +15,22 @@ cp -R "$HOME/Library/Preferences/$phpstorm" "./pref"
 rm -R "./pref/$phpstorm/tasks"
 # Remove recent directories (since this will be new)
 rm "./pref/$phpstorm/options/recentProjectDirectories.xml"
-echo "$phpstorm settings copied"
+echo "$phpstorm settings copied."
 
 # Tunnelblick configurations
 cp -R "$HOME/Library/Application Support/Tunnelblick/Configurations/" "./pref/Tunnelblick"
-echo "Tunnelblick settings copied"
+echo "Tunnelblick settings copied."
 
 # Alfred settings
 cp -R "$HOME/Library/Application Support/Alfred 3" "./pref"
-echo "Alfred settings copied"
+echo "Alfred settings copied."
 
 # Sublime Text 3 settings
 cp -R "$HOME/Library/Application Support/Sublime Text 3" "./pref"
-echo "Sublime Text 3 settings copied"
+echo "Sublime Text 3 settings copied."
 
 # dnsmasq
 # see: https://passingcuriosity.com/2013/dnsmasq-dev-osx/
 cp "/usr/local/etc/dnsmasq.conf" "./pref/dnsmasq/"
 cp -R "/etc/resolver" "./pref/dnsmasq"
-echo "dnsmasq settings copied"
+echo "dnsmasq settings copied."
