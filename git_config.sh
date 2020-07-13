54a21bfcecaf81f417329161d70a0aa921f55269
Tim Kleyersburg <timkley@gmail.com>
2020-07-13 09:24:15 +0000

    Deactivate the default branch pager

--- git_config.sh
+++ git_config.sh
@@ -6,5 +6,8 @@ git config --global user.email you@example.com
 # So you can create and track the remote branch by default with doing git push -u
 git config --global push.default current
 
+# Deactivate the default branch pager
+git config --global pager.branch false
+
 # Set a global excludesfile (which should be copied by the prepare-script)
 git config --global core.excludesfile '~/.gitignore_global'
